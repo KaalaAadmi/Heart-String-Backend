2399725993877082e7b5c760d11746c1a7d6877d
KaalaAadmi <arnav264@gmail.com>
2023-07-22 03:02:14 +0100

    messages endpoints

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,5 +1,5 @@
-import Message from "../models/Message.js";
-import User from "../models/User.js";
+import Message from "./../models/Message.js";
+import User from "./../models/User.js";
 
 export const uploadMessages = async (req, res) => {
 	try {
--- routes/messages.js
+++ routes/messages.js
@@ -3,8 +3,8 @@ import { getMessages, getUserDetails, uploadMessages } from "../controllers/mess
 import multer from "multer";
 
 const router = express.Router();
-const upload =multer({storage:storage})
-router.post('/messages',upload.single('imageFile'),uploadMessages)
+// const upload =multer({storage:storage})
+// router.post('/messages',upload.single('imageFile'),uploadMessages)
 
 router.get('/user/:id',getUserDetails)
 router.get("/:senderId/:recipientId", getMessages);
